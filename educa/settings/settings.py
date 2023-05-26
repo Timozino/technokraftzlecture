43d22a5f941dce35e2c668c652a0ec7201a1a0f7
Folorunso <mod.timson@gmail.com>
2023-05-27 00:35:57 +0100

    modified some changes

--- educa/settings/settings.py
+++ educa/settings/settings.py
@@ -106,23 +106,23 @@ ASGI_APPLICATION = 'educa.asgi.application'
 
 
 # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.sqlite3',
- 'NAME': BASE_DIR / 'db.sqlite3',
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# DATABASES = {
+#  'default': {
+#  'ENGINE': 'django.db.backends.sqlite3',
+#  'NAME': BASE_DIR / 'db.sqlite3',
+#  }
+# }
 
 db_from_prod= dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_prod)
